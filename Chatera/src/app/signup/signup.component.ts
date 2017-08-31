b97e3c4f746c5e4d7a81133228e94ccfe154f520
Damien <dtohin@outlook.fr>
2017-08-31 23:33:46 +0100

    I terminated the signup component

--- Chatera/src/app/signup/signup.component.ts
+++ Chatera/src/app/signup/signup.component.ts
@@ -1,13 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+//import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
+
+import { moveIn, fallIn } from '../router.animations';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  animations: [moveIn(), fallIn()],
+  host: {'[@moveIn]': ''}
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  state: string = '';
+  error: any;
+
+  constructor(public afAuth: AngularFireAuth,private router: Router) { }
+
+  ///When the user finishes to input his credentials for signing in
+  onSubmit(formData) {
+    if(formData.valid) {
+      
+      this.afAuth.auth.signInWithEmailAndPassword(formData.value.email, formData.value.password).then(
+        (success) => {
+        this.router.navigate(['/members']);
+      }).catch(
+        (err) => {
+        this.error = err;
+      });
+    }
+  }
 
   ngOnInit() {
   }
