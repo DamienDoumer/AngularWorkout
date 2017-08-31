3bb5e9b1e414316bcc920af8dc2a87cac22296e9
Damien <dtohin@outlook.fr>
2017-08-31 22:21:33 +0100

    I added the various components needd

--- Chatera/src/app/app.module.ts
+++ Chatera/src/app/app.module.ts
@@ -2,12 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '', 
+  storageBucket: '',
+  messageingSenderId: ''
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    EmailComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
