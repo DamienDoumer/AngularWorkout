043a3aa9b178cc6681c0218a5d69ac7f340a2677
Damien <dtohin@outlook.fr>
2017-09-01 09:46:18 +0100

    I completed the login tutorial, Now I need to test.

--- Chatera/src/app/app.module.ts
+++ Chatera/src/app/app.module.ts
@@ -11,14 +11,17 @@ import { LoginComponent } from './login/login.component';
 import { EmailComponent } from './email/email.component';
 import { SignupComponent } from './signup/signup.component';
 import { MembersComponent } from './members/members.component';
+import { AuthGuard } from './auth.service';
+import { routes } from './app.routes';
 
 export const firebaseConfig = {
 
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '', 
-  storageBucket: '',
-  messageingSenderId: ''
+  apiKey: 'AIzaSyCgIMBkwydHK1YjqWu7-37438wKP1cm7Es',
+  authDomain: 'coherent-voice-171218.firebaseapp.com',
+  databaseURL: 'https://coherent-voice-171218.firebaseio.com', 
+  storageBucket: 'coherent-voice-171218.appspot.com',
+  projectId: "coherent-voice-171218",
+  messageingSenderId: "195976370009"
 }
 
 @NgModule({
@@ -34,9 +37,10 @@ export const firebaseConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    routes
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
